11587e38fcf02179ee2b787703178fec1419bfbf
mac-moosa <embaquie@gmail.com>
2017-06-16 12:41:03 -0400

    added a procfile

---
+++ config.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Moosa on 2017-06-16.
+ */
+
+module.exports = {
+    PORT: process.env.SIMPLE_DEMO_PORT,
+    API_KEY: process.env.DEMO_AUTHY_API_KEY,
+    SECRET: "SUPERSECRETSECRET"
+};
